160c5539c0982055624b8d22bda7405a44dc0533
andrew-archer <razandale@gmail.com>
2022-06-03 18:15:08 +0400

    Initial project commit

---
+++ src/main/java/com/kirill/retrorestservice/RetroRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kirill.retrorestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kirill/retrorestservice/RetroRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirill.retrorestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
